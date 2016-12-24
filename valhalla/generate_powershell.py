6863ea2137209825d036f9e127adbd002c8b1550
aldo195 <osinger@gmail.com>
2016-12-24 09:03:39 +0200

    PowerShell gets trophy and creates raid file with trophy as name, console output only says raid has started without revealing the trophy value.

--- valhalla/generate_powershell.py
+++ valhalla/generate_powershell.py
@@ -5,9 +5,11 @@ def ps_downloader(raid_type, raid_id, trophy):
 $eicar_url = "http://jenda.krynicky.cz/perl/FileType.pm.txt" \n \
 Invoke-WebRequest $request | \n \
 ConvertFrom-Json | \n \
-Select trophy -OutVariable trophy \n \
-$eicar_file = $env:temp+"\\"+$trophy+".txt" \n \
-Invoke-WebRequest -Uri $eicar_url -OutFile $eicar_file'
+Select trophy -ExpandProperty trophy -OutVariable trophyValue > $null \n \
+$eicar_file = $env:temp+"\\"+$trophyValue+".txt" \n \
+Invoke-WebRequest -Uri $eicar_url -OutFile $eicar_file \n \
+Get-Date -Format g \n \
+Write-Host "Raid launch completed." '
     else:
         ps = "Type not supported"
 
